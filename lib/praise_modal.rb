d608d9500685e4e9b557e3c6c84f68cd0153f6db
Laura Kajpust <laura.kajpust@iorahealth.com>
2019-11-20 16:29:21 -0600

    Fix file path

--- lib/praise_modal.rb
+++ lib/praise_modal.rb
@@ -5,7 +5,7 @@ class PraiseModal
     token = ENV["slack_bot_auth"]
     begin
       Rails.logger.info("Opening praise modal")
-      file = File.read('assets/dialog.json')
+      file = File.read(Rails.root + 'lib/assets/dialog.json')
       options = {
         :body => {
           token: token,
