09cece5d4cdcafb7506232c07f6b0c06af6901a0
Laura Kajpust <laura.kajpust@iorahealth.com>
2019-11-20 16:36:50 -0600

    Correct token issue

--- lib/praise_modal.rb
+++ lib/praise_modal.rb
@@ -4,20 +4,23 @@ class PraiseModal
   def self.open(trigger_id)
     token = ENV["slack_bot_auth"]
     begin
-      Rails.logger.info("Opening praise modal")
+      Rails.logger.info("Opening praise modal.")
       file = File.read(Rails.root + 'lib/assets/dialog.json')
       options = {
         :body => {
-          token: token,
+          :token => token,
           :trigger_id => trigger_id,
           :dialog => JSON.parse(file)
         }.to_json,
         :headers => {
-          'Content-Type' => 'application/json',
-          'Accept' => 'application/json'
+          'Content-Type' => 'application/json; charset=utf-8',
+          'Accept' => 'application/json',
+          'Authorization' => "Bearer #{token}"
         }
       }
       url = 'https://slack.com/api/dialog.open'
+      Rails.logger.info("Sending to Slack...")
+      puts options.to_json
       response = HTTParty.post(url, options)
       puts response.to_json
     rescue => e
