532c23e58feee40a17a9d87e952ef8609a2c3a3c
Laura Kajpust <laura.kajpust@iorahealth.com>
2019-11-21 15:50:10 -0600

    Form updates

--- app/controllers/api/praise_controller.rb
+++ app/controllers/api/praise_controller.rb
@@ -4,14 +4,18 @@ class  Api::PraiseController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
+    puts params
     case
     when params[:trigger_id]
       # trigger_id sent by /praise
       PraiseModal.open(params[:trigger_id])
-    when params[:payload][:trigger_id]
+    when params[:payload]
       # trigger_id sent by action
       trigger_id = params[:payload][:trigger_id]
       PraiseModal.open(trigger_id)
+    end
+    when params[:command]=="submit_praise"
+      # recieving response
     end
   end
 end
