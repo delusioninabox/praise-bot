bff3cd292761c48a40796880119b57066c208be2
Laura Kajpust <laura.kajpust@iorahealth.com>
2019-11-25 12:35:39 -0600

    Add block and action ID names

--- app/controllers/api/praise_controller.rb
+++ app/controllers/api/praise_controller.rb
@@ -4,18 +4,13 @@ class  Api::PraiseController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    puts params.as_json
     case
+    when params[:type]=="view_submission" && params[:callback_id]=="submit_praise"
+      # recieved submission
+      puts "submission found"
     when params[:trigger_id]
       # trigger_id sent by /praise
       PraiseModal.open(params[:trigger_id])
-    when params[:payload]
-      payload = params[:payload]
-      # trigger_id sent by action
-      trigger_id = params[:payload][:trigger_id]
-      PraiseModal.open(trigger_id)
-    when params[:type]=="view_submission" && params[:callback_id]=="submit_praise"
-      # recieved submission
     end
   end
 end
