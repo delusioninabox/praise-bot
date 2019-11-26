a19d2b22552ca5dbc6595a979314fb512e2b8827
Laura Kajpust <laura.kajpust@iorahealth.com>
2019-11-26 14:10:06 -0600

    Check for trigger first

--- app/controllers/api/praise_controller.rb
+++ app/controllers/api/praise_controller.rb
@@ -12,15 +12,15 @@ class  Api::PraiseController < ApplicationController
       view = payload['view']
     end
     case
+    when params[:trigger_id]
+      # trigger_id sent by /praise
+      PraiseModal.open(params[:trigger_id])
     when payload['type']=="block_actions"
       # save selects and other interactive elements
       TakeResponse.save(payload['actions'], view['id'], payload['user'])
     when payload['type']=="view_submission" && view['callback_id']=="submit_praise"
       # recieved submission
       TakeResponse.format(view['state']['values'], view['id'], payload['user'])
-    when params[:trigger_id]
-      # trigger_id sent by /praise
-      PraiseModal.open(params[:trigger_id])
     end
   end
 end
