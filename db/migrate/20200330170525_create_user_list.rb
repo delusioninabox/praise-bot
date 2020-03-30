38ec8345a73d07dc4d2a0333d2bdb542b73d94c4
Laura Kajpust <laura.kajpust@iorahealth.com>
2020-03-30 12:07:43 -0500

    Create user table

---
+++ db/migrate/20200330170525_create_user_list.rb
@@ -0,0 +1,11 @@
+class CreateUserList < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :slack_id
+      t.string :display_name
+      t.string :actual_name
+      t.boolean :is_group
+    end
+    add_index :users, :slack_id, unique: true
+  end
+end
