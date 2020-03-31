ffcfca291443dd48586a61dc4e79927a02325dbd
Laura Kajpust <laura.kajpust@iorahealth.com>
2020-03-31 15:51:33 -0500

    map slack user group list and save data

--- lib/team_members.rb
+++ lib/team_members.rb
@@ -5,12 +5,14 @@ class TeamMembers
 
   def self.syncUsers(page)
     @data = SlackConnect.get('users.list', limit, page)
+
     @data.members.map { |item|
       user = User.where(slack_id: item.id).first_or_initialize
       user.assign_attributes({ display_name: item.name, actual_name: item.profile.real_name_normalized, is_group: false })
       return user
     }
     @data.members.save
+
     unless @data.response_metadata.next_cursor.empty?
       # if next_cursor (page) exists
       this.syncUsers(@data.response_metadata.next_cursor)
@@ -19,12 +21,14 @@ class TeamMembers
 
   def self.syncUserGroups(page)
     @data = SlackConnect.get('usergroups.list', limit, page)
-    # loop through data.usergroups
-    # id => slack_id
-    # name => display_name
-    # actual_name is null
-    # is_group is true
-    # save as user
+
+    @data.usergroups.map { |item|
+      user = User.where(slack_id: item.id).first_or_initialize
+      user.assign_attributes({ display_name: item.name, actual_name: null, is_group: true })
+      return user
+    }
+
+    @data.usergroups.save
     # user groups api does not have pagination (at this time)
   end
 end
