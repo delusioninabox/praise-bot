87a8f3b269b8b673c1ff6e4664a674913b850ee7
Laura Kajpust <laura.kajpust@iorahealth.com>
2020-04-06 13:22:38 -0500

    Make slack verification reusable

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,7 +1,17 @@
+require 'slack_verification'
+
 class Api::UserController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    # Skip validation in tests
+    unless Rails.env.test?
+      # Is this a valid Slack request?
+      if SlackVerification.invalid_signature!
+        return
+      end
+    end
+
     search = params[:query]
     if search.present?
       @list = User.where("display_name ILIKE '%#{search}%' OR actual_name ILIKE '%#{search}%'").order('display_name asc')
